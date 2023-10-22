4ce48b39f69e99421f0d26d9b9b13da09f2797ca
gordonotieno <otienogordon95@gmail.com>
2023-10-22 20:21:37 +0300

    create order microservice

---
+++ order-service/src/test/java/com/gordon/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gordon.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
