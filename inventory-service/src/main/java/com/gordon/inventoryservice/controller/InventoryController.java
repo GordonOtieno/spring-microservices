4c7be1d0875bd72c17f35fdb4fdae36339d27cbf
gordonotieno <otienogordon95@gmail.com>
2023-10-24 14:30:00 +0300

    Implemented git web client configurations

--- inventory-service/src/main/java/com/gordon/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/gordon/inventoryservice/controller/InventoryController.java
@@ -1,20 +1,24 @@
 package com.gordon.inventoryservice.controller;
 
+import com.gordon.inventoryservice.dto.InventoryResponse;
 import com.gordon.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/inventory")
+@RequestMapping("/api/v1/inventories")
 @RequiredArgsConstructor
 public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
